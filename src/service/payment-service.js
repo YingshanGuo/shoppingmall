71bb39f980a961cd44751d9f5b99a4d9ed247fd5
YingshanGuo <nana_312005071@qq.com>
2017-07-13 16:44:13 +0800

    payment

---
+++ src/service/payment-service.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2017-06-29 15:48:18 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2017-07-13 16:26:16
+ */
+
+'use strict';
+
+var _mm = require('util/mm.js');
+
+var _payment = {
+    // 获取支付信息
+    getPaymentInfo: function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/pay.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取订单状态
+    getPaymentStatus: function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/query_order_pay_status.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    }
+}
+module.exports = _payment;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: Yoko
  * @Date: 2017-06-27 17:27:38 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2017-07-13 14:43:41
+ * @Last Modified time: 2017-07-13 16:05:18
  */
 var webpack           = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -34,6 +34,7 @@ var config = {
          'order-confirm':['./src/page/order-confirm/index.js'],
          'order-list':['./src/page/order-list/index.js'],
          'order-detail':['./src/page/order-detail/index.js'],
+         'payment':['./src/page/payment/index.js'],
          'user-login':['./src/page/user-login/index.js'],
          'user-register':['./src/page/user-register/index.js'],
          'user-pass-reset':['./src/page/user-pass-reset/index.js'],
@@ -83,6 +84,7 @@ var config = {
          new HtmlWebpackPlugin (getHtmlConfig('order-confirm','订单确认页')),
          new HtmlWebpackPlugin (getHtmlConfig('order-list','订单列表')),
          new HtmlWebpackPlugin (getHtmlConfig('order-detail','订单详情')),
+         new HtmlWebpackPlugin (getHtmlConfig('payment','订单支付')),
          new HtmlWebpackPlugin (getHtmlConfig('user-login','用户登录')),
          new HtmlWebpackPlugin (getHtmlConfig('user-register','用户注册')),
          new HtmlWebpackPlugin (getHtmlConfig('user-pass-reset','找回密码')),
